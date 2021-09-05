7c6ad941c0bc039e0b9c85ac4be4854c0350c99d
Deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-05 16:11:54 +0530

    Lower Case/Preview/Word lenth & Charater Length logic added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,19 +6,35 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
     }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        }
     const handleOnChange = (event) => {
         // console.log("on Change"+ text);
         setText(event.target.value);
         }
-     const [text, setText] = useState('Enter Your Text Here')
+     const [text, setText] = useState('')
     return (
-        <div>
+        <>
+        <div className="container">
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                 {/* <label for="myBox" className="form-label">Example Text Area</label> */}
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1"  onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
+
         </div>
+        
+        <div className="container" mu >
+            <h2>Your Text Summary</h2>
+            <p> {text.split(" ").length} words and {text.length} charaters</p>
+            <p>{.008 * text.split(" ").length} words read</p>
+            <h3>Preview</h3>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
