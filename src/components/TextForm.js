daa5c1ddedc0741a30b189aeb156f049bb2d2d02
Deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-06 16:10:03 +0530

    Copy button and Clear Extra Spaces button Added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,14 +10,34 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
         }
+
         const handleClearClick = () => {
             let newText = ' ';
             setText(newText);
-            }    
-    const handleOnChange = (event) => {
+            } 
+
+        const handleCopyClick = () => {
+            var text = document.getElementById("myBox")
+            text.select();
+            navigator.clipboard.writeText(text.value);
+            }
+
+        const handleExtraSpacesClick = () => {
+            let newText = text.split(/[ ]+/);
+            setText(newText.join(' '))
+                }
+            
+
+        // const handleBoldClick = () => {
+        //     let newText = text.bold();
+        //     setText(newText);
+        //     }  
+
+        const handleOnChange = (event) => {
         // console.log("on Change"+ text);
         setText(event.target.value);
         }
+
      const [text, setText] = useState('')
     return (
         <>
@@ -30,6 +50,9 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1"  onClick={handleUpClick}>Convert to UpperCase</button>
             <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpacesClick}>Clear Extra Space</button>
+            {/* <button className="btn btn-primary mx-1" onClick={handleBoldClick}>Bold Text</button> */}
             
         </div>
         
