5f882b9e56148ac6152111c37ad086d665f204a4
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-10 19:58:44 +0530

    Final Changes - App Completed

--- src/App.js
+++ src/App.js
@@ -2,30 +2,64 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert'
+import About from './components/About'
+import {BrowserRouter as Router,
+       Switch,
+       Route
+     } from "react-router-dom";
+
 
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message ,type) => {
+   setAlert({
+     msg : message,
+    type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert('Dark Mode has been enabled' ,'success');
+      // document.title = 'TextUtils - Dark Mode';
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode has been enabled' ,'success');
+      // document.title = 'TextUtils - Light Mode';
     }
   }
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    <Switch>
+      {/* /users---> Component-1,
+      /users/home---> -Component-2 */}
+          <Route exact path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route exact path="/"> 
+          <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>  
+          </Route>
+    </Switch>
     </div>
-    </> 
+    </Router>
+    </>
   );
 }
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,31 +1,37 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
-    const handleUpClick = () => {
+       const handleUpClick = () => {
     // console.log("New Text Shown" + text);
-    let newText = text.toUpperCase();
-    setText(newText);
+            let newText = text.toUpperCase();
+            setText(newText);
+            props.showAlert('Converted to UpperCase', 'success')
     }
-    const handleLowClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText);
+        const handleLowClick = () => {
+            let newText = text.toLowerCase();
+            setText(newText);
+            props.showAlert('Converted to LowerCase', 'success')
         }
 
         const handleClearClick = () => {
             let newText = ' ';
-            setText(newText);
+            setText(newText)
+            props.showAlert('Text-Box is Clear', 'success')
             } 
 
         const handleCopyClick = () => {
-            var text = document.getElementById("myBox")
-            text.select();
+            // var text = document.getElementById("myBox")
+            // text.select();
             navigator.clipboard.writeText(text.value);
+            document.getSelection().removeAllRanges()
+            props.showAlert('Copied to Clipboard', 'success')
             }
 
         const handleExtraSpacesClick = () => {
             let newText = text.split(/[ ]+/);
             setText(newText.join(' '))
-                }
+            props.showAlert('Extra Spaces removed', 'success')
+        }
             
 
         // const handleBoldClick = () => {
@@ -42,26 +48,29 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color:props.mode === 'dark' ? 'white' : '#042743' }}>
-                <h1>{props.heading}</h1>
+                <h1 className="mb-4">{props.heading}</h1>
                 <div className="mb-3">
                 {/* <label for="myBox" className="form-label">Example Text Area</label> */}
-                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode === 'dark' ? 'grey' : 'white', color:props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} 
+                          style={{backgroundColor:props.mode === 'dark' ? '#13466e' : 'white', 
+                          color:props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8">
+                </textarea>
             </div>
-            <button className="btn btn-primary mx-1"  onClick={handleUpClick}>Convert to UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopyClick}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpacesClick}>Clear Extra Space</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-2"  onClick={handleUpClick}>Convert to UpperCase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleCopyClick}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-2" onClick={handleExtraSpacesClick}>Clear Extra Space</button>
             {/* <button className="btn btn-primary mx-1" onClick={handleBoldClick}>Bold Text</button> */}
             
         </div>
         
         <div className="container" mu style={{color:props.mode === 'dark' ? 'white' : '#042743' }}>
             <h2>Your Text Summary</h2>
-            <p> {text.split(" ").length} words and {text.length} charaters</p>
-            <p>{.008 * text.split(" ").length} words read</p>
+            <p> {text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} charaters</p>
+            <p>{.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} words read</p>
             <h3>Preview</h3>
-            <p>{text.length> 0 ? text :'Enter the text in Textbox above to preview here'}</p>
+            <p>{text.length> 0 ? text :'Nothing to preview'}</p>
         </div>
         </>
     )
